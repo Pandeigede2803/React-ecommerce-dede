d83417a18861b07d9001807dc92bbd54c84f4fbb
mediatechid <adi16.agung@gmail.com>
2023-02-08 20:49:26 +0800

    fix cart

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
 import { PageHero, StripeCheckout } from '../components'
-
-// extra imports
-
+import './checkout.css'
 import { useCartContext } from '../context/cart_context'
 import { Link } from 'react-router-dom'
 
@@ -13,7 +10,7 @@ const CheckoutPage = () => {
   return (
     <main>
       <PageHero title='checkout' />
-      <Wrapper className='page'>
+      <div className='page checkout'>
         {cart.length < 1 ? (
           <div className='empty'>
             <h2>Your cart is empty</h2>
@@ -24,16 +21,10 @@ const CheckoutPage = () => {
         ) : (
           <StripeCheckout />
         )}
-      </Wrapper>
+      </div>
     </main>
   )
 }
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  .empty {
-    text-align: center;
-  }
-`
+
+
 export default CheckoutPage
