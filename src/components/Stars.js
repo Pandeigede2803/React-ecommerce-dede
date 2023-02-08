2a7b591a3f54ee37d7e7addbff468be682d99d6d
mediatechid <adi16.agung@gmail.com>
2023-02-08 12:52:49 +0800

    remove build

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
+import './stars.css'
+
 const Stars = ({ stars, reviews }) => {
   const tempStars = Array.from({ length: 5 }, (_, index) => {
     const number = index + 0.5
@@ -17,25 +19,11 @@ const Stars = ({ stars, reviews }) => {
     )
   })
   return (
-    <Wrapper>
+    <div className='stars-container'>
       <div className='stars'>{tempStars}</div>
       <p className='reviews'>({reviews} customer reviews)</p>
-    </Wrapper>
+    </div>
   )
 }
 
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  span {
-    color: #ffb900;
-    font-size: 1rem;
-    margin-right: 0.25rem;
-  }
-  p {
-    margin-left: 0.5rem;
-    margin-bottom: 0;
-  }
-  margin-bottom: 0.5rem;
-`
 export default Stars
